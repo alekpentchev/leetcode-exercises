33d4be8df979cb6a8f8cb153f7ec611286446555
Aleksander Pentchev <alek.pentchev@gmail.com>
2023-06-15 10:20:39 +0200

    53 - maximum subarray

--- 53-maximum-subarray/index.test.ts
+++ 53-maximum-subarray/index.test.ts
@@ -1,13 +1,16 @@
 import { assertEquals } from "https://deno.land/std@0.183.0/testing/asserts.ts";
-import { maxSubArray } from "./index.ts";
+import { maxSubArray, maxSubArray2 } from "./index.ts";
 
 Deno.test("maximumSubarray", () => {
     // Example 1
     assertEquals(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]), 6);
+    assertEquals(maxSubArray2([-2,1,-3,4,-1,2,1,-5,4]), 6);
 
     // Example 2
     assertEquals(maxSubArray([1]), 1);
+    assertEquals(maxSubArray2([1]), 1);
 
     // Example 3
     assertEquals(maxSubArray([5,4,-1,7,8]), 23);
+    assertEquals(maxSubArray2([5,4,-1,7,8]), 23);
 })
--- 53-maximum-subarray/index.ts
+++ 53-maximum-subarray/index.ts
@@ -1,3 +1,4 @@
+// Kadane's algorithm - time complexity O(1), space O(n)
 export function maxSubArray(nums: number[]): number {
     let currentSum = 0
     let largestSum = nums[0]
@@ -9,4 +10,44 @@ export function maxSubArray(nums: number[]): number {
     }
 
     return largestSum
+}
+
+// divide and concquer approach
+// space complexity O(nlogn), slower and more complicated than Kadane's algorithm in this case
+export const maxSubArray2 = (nums: number[]) => {
+    const leftIndex = 0;
+    const rightIndex = nums.length - 1;
+
+    return findMaxSubArraySum(nums, leftIndex, rightIndex);
+};
+
+function findMaxSubArraySum(nums: number[], left: number, right: number): number {
+    if (left > right) {
+        return 0;
+    }
+
+    const middle = Math.floor((left + right) / 2);
+    let currSum = 0;
+    let maxLeftSum = 0;
+    let maxRightSum = 0;
+
+    for (let i = middle - 1; i >= left; i--) {
+        currSum += nums[i];
+        maxLeftSum = Math.max(maxLeftSum, currSum);
+    }
+
+    currSum = 0;
+    for (let i = middle + 1; i <= right; i++) {
+        currSum += nums[i];
+        maxRightSum = Math.max(maxRightSum, currSum);
+    }
+
+    // maxSum contains the middle element + highest sums from both halves
+    let maxSum = nums[middle] + maxLeftSum + maxRightSum;
+
+    // recursion - find max subarray for both halves and so on...
+    const leftHalf = findMaxSubArraySum(nums, left, middle - 1);
+    const rightHalf = findMaxSubArraySum(nums, middle + 1, right);
+
+    return Math.max(maxSum, Math.max(leftHalf, rightHalf));
 }

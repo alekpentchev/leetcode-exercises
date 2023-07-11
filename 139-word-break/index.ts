46996d5410c80b51c21b5a0d66c70ead8c4d2be9
Aleksander Pentchev <alek.pentchev@gmail.com>
2023-07-11 12:15:58 +0200

    139 - word break - small optimization

--- 139-word-break/index.ts
+++ 139-word-break/index.ts
@@ -1,11 +1,10 @@
 export function wordBreak(s: string, wordDict: string[]): boolean {
-    const wordSet = new Set(wordDict)
     const dp = new Array(s.length + 1)
     dp[0] = true
 
     for (let i = 1; i < s.length + 1; i++) {
         for (let j = 0; j < i; j++) {
-            if (dp[j] && wordSet.has(s.substring(j, i))) {
+            if (dp[j] && wordDict.includes(s.slice(j, i))) {
                 dp[i] = true
                 break
             }
